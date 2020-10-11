2cf9ff42d052093eac1bad9bd0ed16fc95b6f61e
Ye Zhang <mr.yezhang@gmail.com>
2020-10-11 12:13:52 -0700

    1. Fix MQTT client parameter mis-match bug. 2. Turned on MQTT and it works!

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ _heater_off_temperature = const(23)
 _heater_on_temperature = 22.9
 
 # In ms.
-_measure_interval = const(1000)
+_measure_interval = const(5000)
 assert _measure_interval > 100, "Must wait for measurement settle time before taking the next measurement."
 
 with open('credentials.json') as f:
@@ -29,6 +29,7 @@ mqtt_user = mqtt['user']
 mqtt_pwd = mqtt['pwd']
 mqtt_client_id = mqtt['client_id']
 mqtt_server = mqtt['server']
+mqtt_topic = mqtt['topic']
 
 while True:
     time.sleep_ms(_measure_interval)
@@ -39,26 +40,26 @@ while True:
     temperature = measurement["temperature"]
     temperature_message = 'Temperature: {}'.format(temperature)
     print(temperature_message)
-    #mqtt_client.publish_message(temperature_message, mqtt_client_id, mqtt_server, mqtt_user, mqtt_pwd)
+    mqtt_client.publish_message(mqtt_topic, temperature_message, mqtt_client_id, mqtt_server, mqtt_user, mqtt_pwd)
 
     if temperature <= _heater_on_temperature:
         heater.turn_on()
-        message = 'heater turned on'
+        message = 'Heater turned on'
         print(message)
-        # mqtt_client.publish_message(message, mqtt_client_id, mqtt_server, mqtt_user, mqtt_pwd)
+        mqtt_client.publish_message(mqtt_topic, message, mqtt_client_id, mqtt_server, mqtt_user, mqtt_pwd)
     elif temperature >= _heater_off_temperature:
         heater.turn_off()
-        message = 'heater turned off'
+        message = 'Heater turned off'
         print(message)
-        # mqtt_client.publish_message(message, mqtt_client_id, mqtt_server, mqtt_user, mqtt_pwd)
+        mqtt_client.publish_message(mqtt_topic, message, mqtt_client_id, mqtt_server, mqtt_user, mqtt_pwd)
 
     humidity = measurement["humidity"]
     humidity_message = 'Humidity: {}'.format(humidity)
     print(humidity_message)
-    # mqtt_client.publish_message(humidity_message, mqtt_client_id, mqtt_server, mqtt_user, mqtt_pwd)
+    mqtt_client.publish_message(mqtt_topic, humidity_message, mqtt_client_id, mqtt_server, mqtt_user, mqtt_pwd)
 
     pressure = measurement["pressure"]
     pressure_message = 'Pressure: {}'.format(pressure)
     print(pressure_message)
-    #mqtt_client.publish_message(pressure_message, mqtt_client_id, mqtt_server, mqtt_user, mqtt_pwd)
+    mqtt_client.publish_message(mqtt_topic, pressure_message, mqtt_client_id, mqtt_server, mqtt_user, mqtt_pwd)
     print('========================')
--- mqtt_client.py
+++ mqtt_client.py
@@ -1,11 +1,12 @@
 from MicroPythonLibriaries.umqtt.simple import MQTTClient
 
 
-def publish_message(message, client_id, server, user, password) -> None:
+def publish_message(topic, message, client_id, server, user, password) -> None:
     """
     Publish message using MQTT.
     Only call this method when wifi is connected.
-    :param message: The message to be published, in binary format.
+    :param topic: The topic the message belongs to, in binary format. e.g. b"my_topic"
+    :param message: The message to be published, in binary format. e.g. b"my_message"
     :param client_id: String client id of this client.
     :param server: Server ip address as string.
     :param user: User name to authenticate with MQTT server, e.g. b"mosquitto"
@@ -14,13 +15,14 @@ def publish_message(message, client_id, server, user, password) -> None:
     """
     print('Publish message {0}'.format(message))
     # don't catch "fail fast fail hard".
-    c = MQTTClient(client_id,
-                   server,
-                   user,
-                   password,
+    c = MQTTClient(client_id=client_id,
+                   # use parameter assignments, because skipped port and thus sequence won't match.
+                   server=server,
+                   user=user,
+                   password=password,
                    ssl=False)
     if 0 == c.connect():  # 0 is success.
-        c.publish(b"network_watchdog_status_topic", message)
+        c.publish(topic, message)
         c.disconnect()
     else:
         print('Connect to MQTT server failed. ')
